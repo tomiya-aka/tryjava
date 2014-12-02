f5935dc44de2af5b2a247e693f9f991a08ff92cd
tomiya-aka <1310990006h@hiro.kindai.ac.jp>
2014-12-02 18:45:42 +0900

    練習5-1
    メソッド　introduseOneselfの実装

--- src/lesson5/Q1.java
+++ src/lesson5/Q1.java
@@ -1,8 +1,14 @@
 package lesson5;
 
 public class Q1 {
-	public static void main (String[] args) {
-		
+	public static void introduceOneself(){
+		String name = "ãk";
+		int age=20;
+		double height = 170;
+		char gender = 'íj';
+		System.out.println(name + age + "çŒ" + height + "cm" + gender);
 	}
-
+	public static void main(String[] args){
+		introduceOneself();
+	}
 }
