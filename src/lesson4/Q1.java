7ba7c1f9850d9199fd9da2dd0215ec62003fab9c
tomiya-aka <1310990006h@hiro.kindai.ac.jp>
2014-12-02 17:25:22 +0900

    練習4-1

---
+++ src/lesson4/Q1.java
@@ -0,0 +1,11 @@
+package lesson4;
+
+public class Q1 {
+	public static void main (String[] args) {
+		int[] points = new int[4];
+		double[] weights = new double[5];
+		boolean[] answers = new boolean[3];
+		String[] names = new String[3];
+	}
+
+}
